6f324e07cc681ff62d02b5415ed8af52861647f7
Juan David Garrido <juandagarrido@hotmail.com>
2018-04-07 11:50:24 -0500

    Firebase dependencies added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,25 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {FIREBASE_CONFIG} from "./firebase.credentials";
+import {AngularFireModule} from "angularfire2";
+import {AngularFireAuthModule} from "angularfire2/auth";
+import {AngularFireDatabaseModule} from "angularfire2/database";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
     StatusBar,
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDDlt1idqoieJpKmWDKKT-IJnyIdtHvzNk",
+  authDomain: "homihacks.firebaseapp.com",
+  databaseURL: "https://homihacks.firebaseio.com",
+  projectId: "homihacks",
+  storageBucket: "",
+  messagingSenderId: "522868651662"
+};
