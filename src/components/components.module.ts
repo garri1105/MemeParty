b498a7d3103c35c4fb8c92dd2a4be6b847e5e4fc
Nathan Oelke <oelke047@umn.edu>
2018-04-07 15:04:29 -0500

    Added lobby buttons

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CreateJoinRoomComponent } from './create-join-room/create-join-room';
 import { PlayerListComponent } from './player-list/player-list';
+import { LobbyButtonsComponent } from './lobby-buttons/lobby-buttons';
 @NgModule({
 	declarations: [CreateJoinRoomComponent,
-    PlayerListComponent],
+    PlayerListComponent,
+    LobbyButtonsComponent],
 	imports: [],
 	exports: [CreateJoinRoomComponent,
-    PlayerListComponent]
+    PlayerListComponent,
+    LobbyButtonsComponent]
 })
 export class ComponentsModule {}
--- src/pages/lobby/lobby.module.ts
+++ src/pages/lobby/lobby.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LobbyPage } from './lobby';
 import { PlayerListComponent } from '../../components/player-list/player-list'
+import { LobbyButtonsComponent } from '../../components/lobby-buttons/lobby-buttons'
 
 @NgModule({
   declarations: [
     LobbyPage,
-    PlayerListComponent
+    PlayerListComponent,
+    LobbyButtonsComponent
   ],
   imports: [
     IonicPageModule.forChild(LobbyPage),
