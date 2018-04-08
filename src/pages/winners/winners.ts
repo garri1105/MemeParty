77b1a7570df55a6bcfb2fc3f161f4386a8916ee3
Nathan Oelke <oelke047@umn.edu>
2018-04-08 09:10:18 -0500

    Updated winners page

--- src/pages/winners/winners.ts
+++ src/pages/winners/winners.ts
@@ -20,11 +20,11 @@ export class WinnersPage {
               private roomData: RoomDataProvider) {
 
     this.roomId = this.navParams.get('roomId');
-    //this.getPlayersFromRoom();
-    this.players = [{name: "Joe", score:42} as Player,
-                   {name: "Teddy", score:12} as Player,
-                   {name: "Nancy", score:5} as Player]
-    //console.log(this.players)
+    this.getPlayersFromRoom();
+    // this.players = [{name: "Joe", score:42} as Player,
+    //                {name: "Teddy", score:12} as Player,
+    //                {name: "Nancy", score:5} as Player]
+    console.log(this.players)
   }
 
   getPlayersFromRoom() {
@@ -37,7 +37,6 @@ export class WinnersPage {
       });
 
     // Sort the players according to score from highest to lowest
-
   }
 
 }
