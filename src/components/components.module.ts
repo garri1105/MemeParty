8f8b9520808d7cee2024e0ab9926d37bc2c61d79
Juan David Garrido <juandagarrido@hotmail.com>
2018-04-07 23:09:25 -0500

    Deleted import

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CreateJoinRoomComponent } from './create-join-room/create-join-room';
 import { PlayerListComponent } from './player-list/player-list';
-import { LobbyButtonsComponent } from './lobby-buttons/lobby-buttons';
 import { MemeTilesComponent } from './meme-tiles/meme-tiles';
+
 @NgModule({
 	declarations: [CreateJoinRoomComponent,
     PlayerListComponent,
