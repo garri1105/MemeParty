54fda527c9a7132a48717f1c7664ac922e4f753c
Kevin Trinh <ktrinh@bowdoin.edu>
2018-04-07 21:42:04 -0500

    captioning page now has textbox, submit button, and countdown timer.

--- src/pages/captioning/captioning.ts
+++ src/pages/captioning/captioning.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the CaptioningPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-captioning',
@@ -15,11 +8,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CaptioningPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	time:string;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CaptioningPage');
-  }
+	constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+	// Set countdown reference time
+	var countDownOrigin = new Date().getTime();
+	var that = this;
+
+	// Update the count down every 1 second
+	var x = setInterval(function() {
+
+		// Get todays date and time
+		var now = new Date().getTime();
+
+		// Find the distance between now an the count down date
+		var distance = Math.floor((countDownOrigin + 45000 - now)/1000);
+
+		that.time = distance + "s";
+
+  		// If the count down is finished, write some text 
+  		if (distance < 0) {
+    	clearInterval(x);
+    	document.getElementById("demo").innerHTML = "SUBMISSIONS OVER!";
+  		}
+  		console.log(this.time);
+		}, 1000);
+
+	}
+
+	ionViewDidLoad() {
+    	console.log('ionViewDidLoad CaptioningPage');
+  	}
+
 
 }
+
