144343ee67a07c69fbe7fa976c3a5a3bf3d841fb
Nathan Oelke <oelke047@umn.edu>
2018-04-08 02:39:28 -0500

    Updated lobby buttons

--- src/pages/lobby/lobby.ts
+++ src/pages/lobby/lobby.ts
@@ -14,21 +14,31 @@ export class LobbyPage {
   player: Player;
   roomId: string;
   lobbyImage: string;
+  selectedImage: boolean;
+  submittedImage: boolean;
   getData: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.player = this.navParams.get("player");
     this.roomId = this.navParams.get("roomId");
     this.lobbyImage = "assets/imgs/placeholder.png";
+    this.selectedImage = false;
+    this.selectedImage = false;
     let that = this;
 
     this.getData = function(data) {
       return new Promise((resolve, reject) => {
         that.lobbyImage = "assets/memelibrary/" + data;
-        console.log(that.lobbyImage);
+        that.selectedImage = true;
         resolve();
       });
     };
+  }
+
+  submit() {
+    // Confirm the selected image and add it to the room
+
+    this.submittedImage = true;
   }
 
   toPhotoSelection() {
